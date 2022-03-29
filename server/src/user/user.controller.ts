e03c87c116df3b42b909d359959e35e092dc1304
Charles-Elie Hervy <charleseliehervy@protonmail.com>
2022-03-29 17:17:33 +0200

    Change url api

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -3,26 +3,26 @@ import { CreateUserDto } from './dto/createUser.dto';
 import { GetUserDto } from './dto/getUser.dto';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
-	@Post('/new')
+	@Post('/')
 	async new(@Body() user: CreateUserDto): Promise<GetUserDto> {
 		return this.userService.create(user);
 	}
 
-	@Delete('/deleteById/:id')
+	@Delete('/:id')
 	async remove(@Param('id') id: number): Promise<void> {
 		return this.userService.remove(id);
 	}
 
-	@Get('/getById/:id')
+	@Get('/:id')
 	async getById(@Param('id') id: number) : Promise<GetUserDto> {
 		return this.userService.getById(id);
 	}
 
-	@Get('/getAll')
+	@Get('/')
 	async getAll() : Promise<GetUserDto[]> {
 		return this.userService.getAll();
 	}
